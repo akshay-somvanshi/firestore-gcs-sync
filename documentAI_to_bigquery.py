a5587716b38db2d763a101617565031ff9ee49e9
akshay-somvanshi <akshaysomvanshi18@gmail.com>
2025-10-30 15:11:39 +0100

    Complete working script -> added syncronising with BigQuery

--- documentAI_to_bigquery.py
+++ documentAI_to_bigquery.py
@@ -1,7 +1,10 @@
 from google.cloud import bigquery, documentai_v1, storage
+import uuid
+from google.protobuf.json_format import MessageToJson
+from datetime import datetime
 
 # Variables for bigquery
-project_id = 'dash-beta- e61d0'
+project_id = 'dash-beta-e61d0'
 dataset_id = 'dash_beta_database'
 table = 'document'
 
@@ -15,7 +18,10 @@ mime_type = 'application/pdf'
 # Connect to the clients
 client_bq = bigquery.Client(project=project_id)
 client_storage = storage.Client()
-client_docAi = documentai_v1.DocumentProcessorServiceClient()
+# The client defaults to us endpoint -> must configure to eu. 
+client_docAi = documentai_v1.DocumentProcessorServiceClient(
+    client_options={"api_endpoint": "eu-documentai.googleapis.com"}
+)
 
 # Retrieve the bucket with user files 
 bucket = client_storage.get_bucket(bucker_name)
@@ -34,8 +40,28 @@ for user in users:
 
     # Get the processor path
     processor_name = client_docAi.processor_path(project_id, location, processor_id)
-    # Get the processor reference
-    request = documentai_v1.GetProcessorRequest(processor_name, raw_document=user)
-    processor = client_docAi.get_processor(request)
+
+    # Read file bytes
+    pdf_bytes = user.download_as_bytes()
+
+    # Process with Document AI
+    request = {
+        "name": processor_name,
+        "raw_document": {"content": pdf_bytes, "mime_type": "application/pdf"},
+    }
+
+    result = client_docAi.process_document(request=request)
+    raw_data = type(result).to_json(result)
+    document_id = str(uuid.uuid4())
+
+    row_to_insert = [{
+    "document_id": document_id,
+    "document_type": "electricity bill",
+    "raw_data": raw_data,  # from MessageToJson
+    "uploaded_time": datetime.utcnow().isoformat(),
+    "user_id": user_id
+    }]
+
+    client_bq.insert_rows_json(f"{project_id}.{dataset_id}.{table}", row_to_insert)
 
     
