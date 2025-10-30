d600cdbec99afcb6dfe2713ee8a0060a458f1df4
akshay-somvanshi <akshaysomvanshi18@gmail.com>
2025-10-30 16:40:36 +0100

    Inserting all data created exceeding tokens error for LLM in next stage - trimmed result output

--- documentAI_to_bigquery.py
+++ documentAI_to_bigquery.py
@@ -1,7 +1,9 @@
 from google.cloud import bigquery, documentai_v1, storage
 import uuid
 from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict
 from datetime import datetime
+import json
 
 # Variables for bigquery
 project_id = 'dash-beta-e61d0'
@@ -51,17 +53,39 @@ for user in users:
     }
 
     result = client_docAi.process_document(request=request)
-    raw_data = type(result).to_json(result)
+
+    # Convert to JSON string
+    json_string = type(result).to_json(result)
+
+    # Parse into a Python dict
+    full_json = json.loads(json_string)
+
+    trimmed_json = {
+    "document": {
+        "text": full_json["document"].get("text", ""),
+        }
+    }
+
     document_id = str(uuid.uuid4())
 
     row_to_insert = [{
     "document_id": document_id,
     "document_type": "electricity bill",
-    "raw_data": raw_data,  # from MessageToJson
+    "raw_data": json.dumps(trimmed_json),  # from MessageToJson
     "uploaded_time": datetime.utcnow().isoformat(),
     "user_id": user_id
     }]
 
-    client_bq.insert_rows_json(f"{project_id}.{dataset_id}.{table}", row_to_insert)
+    # truncate = "TRUNCATE TABLE `dash-beta-e61d0.dash_beta_database.document`;"
+    # result = client_bq.query(truncate).result()
+
+    table_ref = client_bq.dataset(dataset_id).table(table)
+    # client_bq.insert_rows_json(table_ref, row_to_insert)
+    errors = client_bq.insert_rows_json(table_ref, row_to_insert)
+    if errors:
+        print("Insert errors:", errors)
+    else:
+        print("Insert successful")
+
 
     
